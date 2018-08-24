860c6e147be80e482bc20350373ea827751e0216
huijuanli <30285223+huijuanl@users.noreply.github.com>
2018-08-24 16:50:32 +0800

    modify hasSubTree

--- exercises/HasSubTree_18.java
+++ exercises/HasSubTree_18.java
@@ -15,26 +15,31 @@ public class HasSubTree_18 {
 	    }
 
 	}
-	public static boolean hasSubtree(TreeNode root1,TreeNode root2) {
-        if(root2==null)
-			  return false;
-        return process(root1,root2);
-       	    
-    }
-    public static boolean process(TreeNode root1,TreeNode root2){
-         if(root1==null&&root2!=null)
-	           return false;
-		 if(root2==null)
-			  return true;
-	     if(root1.val==root2.val)
-	    	   return (
-	    			   (process(root1.left, root2.left)
-	    	            &&process(root1.right, root2.right))
-	    			   )
-	    			   ||process(root1.left, root2)||process(root1.right, root2);
-	       else return
-              process(root1.left, root2)||process(root1.right, root2);
-    }
+	public static boolean isSubtree(TreeNode s, TreeNode t) {//判断t是否是s的子树
+		if(s==null&&t==null)
+			return true;
+		if(s==null)
+			return false;
+		if(t==null)
+			return false;
+		if(s.val!=t.val)
+			return isSubtree(s.left,t)||isSubtree(s.right,t);
+		else {
+			return (process(s.left,t.left)&&process(s.right,t.right))
+					||isSubtree(s.left,t)||isSubtree(s.right,t);
+		}
+
+	}
+	public static boolean process(TreeNode s1,TreeNode t1){//判断s1和t1是否结构和数值完全相等,先序遍历
+		if(t1==null)
+			return true;
+		if(s1==null)
+			return false;
+		if(s1.val!=t1.val)
+			return false;
+		else return (process(s1.left,t1.left)&&process(s1.right,t1.right));
+
+	}
 	  public static void main(String[] args) {
 		    TreeNode head1 = new TreeNode(8);
 	        head1.left = new TreeNode(8);
@@ -44,9 +49,8 @@ public class HasSubTree_18 {
 	        head1.left.right.right = new TreeNode(7);
 	        head1.right = new TreeNode(7);
 	        TreeNode head2 = new TreeNode(8);
-	        head2.left = new TreeNode(9);
 	        head2.right = new TreeNode(2);
-	        System.out.println(hasSubtree(head1, head2));
+	        System.out.println(isSubtree(head1, head2));
 
 	    }
 	
